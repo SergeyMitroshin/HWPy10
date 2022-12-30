a43e36d19a80c1839f784c4d81423433f066542a
SergeyMitroshin <sergey@host-best.com>
2022-12-30 16:09:43 +0300

    first commit

---
+++ phonebook.py
@@ -0,0 +1,142 @@
+import re
+from telegram.ext import Updater, MessageHandler, CommandHandler, Filters
+import csv
+
+# Set the command list for the bot
+commands = ['phonebook']
+
+# Set the phonebook database as a dictionary
+phonebookDB = {'Vasia': '+223322456', 'Petr': '+33245634', 'Daniil': '+7345345345'}
+
+def handle_message(update, context):
+    # Get the message from the update
+    message = update.message
+    text = message.text
+
+    # Remove "abc" from the message text
+    modified_text = remove_abc(text)
+
+    # Send the modified message back to the user
+    context.bot.send_message(chat_id=message.chat_id, text=modified_text)
+
+def start(update, context):
+    #Send a message when the command /start is issued
+    update.message.reply_text('Привет! Я бот - телефонный справочник.\n'
+                              'Поиск: /phonebook search [name]\n'
+                              'Добавление: /phonebook add [name] [phone number]\n'
+                              'Удаление: /phonebook remove [name]\n'
+                              'Экспорт: /export [file name.csv]\n')
+
+
+def phonebook(update, context):
+    #Search and manage the phonebook database.
+    
+    # Get the user's message
+    message = update.message.text
+    
+    # Split the message into a list of words
+    words = message.split()
+    
+    # Check that the message has the correct format (i.e., at least two words: the command and a subcommand)
+    if len(words) >= 2:
+        # Extract the subcommand from the message
+        subcommand = words[1]
+        # Check the subcommand
+        if subcommand == 'search':
+            # Check that the message has the correct format (i.e., three words: the command, the subcommand, and the name to search)
+            if len(words) == 3:
+                # Extract the name to search from the message
+                name = words[2]
+                # Check if the name is in the phonebook
+                if name in phonebookDB:
+                    # Send the phone number back to the user
+                    update.message.reply_text(f'{name}: {phonebookDB[name]}')
+                else:
+                    update.message.reply_text(f'Ошибка: {name} не найдено в телефонной книге')
+            else:
+                update.message.reply_text('Ошибка: неправильный формат\n'
+                                          'Используйте формат /phonebook search [name]\n'
+                                          'для поиска в справочнике')
+        elif subcommand == 'add':
+            # Check that the message has the correct format (i.e., four words: the command, the subcommand, the name, and the phone number)
+            if len(words) == 4:
+                # Extract the name and phone number from the message
+                name = words[2]
+                phone_number = words[3]
+                # Add the name and phone number to the phonebook
+                phonebookDB[name] = phone_number
+                update.message.reply_text(f'{name} добавлено в телефонную книгу')
+            else:
+                update.message.reply_text('Ошибка: неправильный формат\n'
+                                          'Используйте формат /phonebook add [name] [phone number]\n'
+                                          'для добавления контакта в телефонную книгу')
+        elif subcommand == 'remove':
+            # Check that the message has the correct format (i.e., three words: the command, the subcommand, and the name to remove)
+            if len(words) == 3:
+                # Extract the name to remove from the message
+                name = words[2]
+                # Check if the name is in the phonebook
+                if name in phonebookDB:
+                    # Remove the name and phone number from the phonebook
+                    del phonebookDB[name]
+                    update.message.reply_text(f'{name} удалено из телефонной книги')
+                else:
+                    update.message.reply_text(f'Ошибка: {name} не найдено в телефонной книге')
+            else:
+                update.message.reply_text('Ошибка: неправильный формат\n'
+                                          'Используйте формат /phonebook remove [name]\n'
+                                          'для удаления контакта из телефонной книги')
+
+def export_phonebook(update, context):
+    # Get the user's message
+    message = update.message.text
+    
+    # Split the message into a list of words
+    words = message.split()
+    
+    # Check that the message has the correct format (i.e., two words: the command and the file name)
+    if len(words) == 2:
+        # Extract the file name from the message
+        file_name = words[1]
+        
+        # Open the CSV file in write mode
+        with open(file_name, 'w', newline='') as csv_file:
+            # Create a CSV writer object
+            csv_writer = csv.writer(csv_file)
+            
+            # Write the header row to the CSV file
+            csv_writer.writerow(['Name', 'Phone Number'])
+            
+            # Iterate through the phonebook database and write each name and phone number to the CSV file
+            for name, phone_number in phonebookDB.items():
+                csv_writer.writerow([name, phone_number])
+        
+        # Use the telegram API to send the CSV file to the user
+        context.bot.send_document(chat_id=update.effective_chat.id, document=open(file_name, 'rb'))
+    else:
+        update.message.reply_text('Ошибка: неправильный формат\n'
+                         'Используйте формат /export [file name]\n'
+                         'для экспорта телефонной книги в CSV-файл')
+
+def main():
+    # Create the Updater and pass it the bot's token
+    updater = Updater('5800510923:AAGGIu2AuPktr_F6RmeJvwR-pbO-m5Q8D6o', use_context=True)
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+  
+    # Add a message handler that will respond to messages containing "abc"
+    dp.add_handler(MessageHandler(Filters.text & Filters.regex(r"abc"), handle_message))
+    
+    dp.add_handler(CommandHandler('start', start))
+    dp.add_handler(CommandHandler('phonebook', phonebook))
+    dp.add_handler(CommandHandler('export', export_phonebook))
+  
+    # Start the bot
+    updater.start_polling()
+
+    # Run the bot until you press Ctrl-C
+    updater.idle()
+
+if __name__ == "__main__":
+  main()
---
+++ ttt.py
@@ -0,0 +1,126 @@
+import os
+import random
+import sys
+
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, MessageHandler, Filters
+
+import strings as st
+
+
+def newGame(update, _):
+    # сформировать callBack данные для первой игры, то есть строку, состояющую из 9 неопределенных символов
+    data = ''
+    for i in range(0, 9):
+        data += st.SYMBOL_UNDEF
+
+    # отправить сообщение для начала игры
+    update.message.reply_text(st.ANSW_YOUR_TURN, reply_markup=InlineKeyboardMarkup(getKeyboard(data)))
+
+
+
+def isWin(arr, who):
+    if (((arr[0] == who) and (arr[4] == who) and (arr[8] == who)) or
+            ((arr[2] == who) and (arr[4] == who) and (arr[6] == who)) or
+            ((arr[0] == who) and (arr[1] == who) and (arr[2] == who)) or
+            ((arr[3] == who) and (arr[4] == who) and (arr[5] == who)) or
+            ((arr[6] == who) and (arr[7] == who) and (arr[8] == who)) or
+            ((arr[0] == who) and (arr[3] == who) and (arr[6] == who)) or
+            ((arr[1] == who) and (arr[4] == who) and (arr[7] == who)) or
+            ((arr[2] == who) and (arr[5] == who) and (arr[8] == who))):
+        return True
+    return False
+
+
+def countUndefinedCells(cellArray):
+    counter = 0
+    for i in cellArray:
+        if i == st.SYMBOL_UNDEF:
+            counter += 1
+    return counter
+
+
+def game(callBackData):
+    message = st.ANSW_YOUR_TURN  
+    alert = None
+    buttonNumber = int(callBackData[0])  
+    if not buttonNumber == 9:  
+        charList = list(callBackData)  
+        charList.pop(0) 
+        if charList[buttonNumber] == st.SYMBOL_UNDEF:  
+            charList[buttonNumber] = st.SYMBOL_X  
+            if isWin(charList, st.SYMBOL_X):  
+                message = st.ANSW_YOU_WIN
+            else:  
+                if countUndefinedCells(charList) != 0:  # проверка: есть ли свободные ячейки для хода
+                    isCycleContinue = True
+                    while (isCycleContinue):
+                        rand = random.randint(0, 8)  # генерация случайного числа - клетки, в которую сходит бот
+                        if charList[rand] == st.SYMBOL_UNDEF:  # если клетка неопределенна, то ходит бот
+                            charList[rand] = st.SYMBOL_O
+                            isCycleContinue = False  # смена значения переменной для остановки цикла
+                            if isWin(charList, st.SYMBOL_O):  # проверка: выиграл ли бот после своего кода
+                                message = st.ANSW_BOT_WIN
+
+        else:
+            alert = st.ALERT_CANNOT_MOVE_TO_THIS_CELL
+
+        if countUndefinedCells(charList) == 0 and message == st.ANSW_YOUR_TURN:
+            message = st.ANSW_DRAW
+
+        callBackData = ''
+        for c in charList:
+            callBackData += c
+
+    if message == st.ANSW_YOU_WIN or message == st.ANSW_BOT_WIN or message == st.ANSW_DRAW:
+        message += '\n'
+        for i in range(0, 3):
+            message += '\n | '
+            for j in range(0, 3):
+                message += callBackData[j + i * 3] + ' | '
+        callBackData = None  # обнуление callBackData
+
+    return message, callBackData, alert
+
+
+
+
+def getKeyboard(callBackData):
+    keyboard = [[], [], []]  
+    if callBackData != None:  # если
+
+        for i in range(0, 3):
+            for j in range(0, 3):
+                keyboard[i].append(InlineKeyboardButton(callBackData[j + i * 3], callback_data=str(j + i * 3) + callBackData))
+
+    return keyboard
+
+
+def button(update, _):
+    query = update.callback_query
+    callbackData = query.data  # получение callbackData, скрытых в кнопке
+
+    message, callbackData, alert = game(callbackData)  # игра
+    if alert is None:  
+        query.answer()  
+        query.edit_message_text(text=message, reply_markup=InlineKeyboardMarkup(getKeyboard(callbackData)))
+    else:  
+        query.answer(text=alert, show_alert=True)
+
+
+def help_command(update, _):
+    update.message.reply_text(st.ANSW_HELP)
+
+
+if __name__ == '__main__':
+   # updater = Updater(getToken())  # получения токена из файла 'token.txt' и инициализация updater
+    updater = Updater('5800510923:AAGGIu2AuPktr_F6RmeJvwR-pbO-m5Q8D6o')
+    # добавление обработчиков
+    updater.dispatcher.add_handler(CommandHandler('start', newGame))
+    updater.dispatcher.add_handler(CommandHandler('help', help_command))
+    updater.dispatcher.add_handler(MessageHandler(Filters.text, help_command))  # обработчик на любое текстовое сообщение
+    updater.dispatcher.add_handler(CallbackQueryHandler(button))  # добавление обработчика на CallBack кнопки
+
+    # Запуск бота
+    updater.start_polling()
+    updater.idle()
